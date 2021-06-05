279684a2dfb83b42c158a2f710b63b73a22678d2
DESKTOP-A427LQ4\vantan <shutaro0415@outlook.jp>
2021-06-05 13:33:45 +0900

    ブロック崩し

---
+++ Sclipt/Pcontroller.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pcontroller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private GameObject ball;
+    [SerializeField]
+    private GameObject ballPosition;
+
+    static public bool ballExistFlag = false;
+
+    public float accel = 1000.0f;
+    
+    Rigidbody rb;
+  
+
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(transform.right * Input.GetAxis("Horizontal") * accel,ForceMode.Impulse);
+
+        if (Input.GetKeyDown("space"))
+        {
+            if(ballExistFlag == false)
+            {
+                Instantiate(ball,
+                        ballPosition.transform.position,
+                        ballPosition.transform.rotation);
+                ballExistFlag = true;
+
+            }
+            
+        }
+
+    }
+}
---
+++ Sclipt/Sphere.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sphere : MonoBehaviour
+{
+
+    [SerializeField]
+    private float speed = 30.0f;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+        rb.AddForce((transform.right+transform.forward)*speed,ForceMode.VelocityChange);
+
+    }
+
+    // Update is called once per frame
+    
+}
